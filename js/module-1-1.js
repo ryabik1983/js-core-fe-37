03d272990fc576d400120483288d807a6e10c804
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-07-15 10:14:28 +0300

    module-1-1
    module-1-1

---
+++ js/module-1-1.js
@@ -0,0 +1,76 @@
+//Variables
+//ES5
+// console.log(`myFirstVar`, myFirstVar); //undefined
+// var myFirstVar
+
+
+// ES6
+
+// let mySecondVar
+// console.log(`mySecondVar`, mySecondVar); //undefined
+
+// mySecondVar = "var by let"
+// console.log(`mySecondVar`, mySecondVar);
+
+// const myThirdVar = `variable by const`
+// console.log(`myThirdVar`, myThirdVar);
+// myThirdVar = `dfdf`; //нельзя переписать имя переменной обявленной через конст
+
+// Значения переменных
+// Primitives & operator typeof
+// console.log(undefined);
+// console.log(typeof undefined);
+
+// console.log(`Ryabovol`);
+// console.log(typeof `Ryabovol`);
+
+// console.log(100.12345);
+// console.log(typeof 100.12345);
+
+// console.log(true);
+// console.log(typeof true);
+
+// console.log(false);
+// console.log(typeof false);
+
+// console.log(null);
+// console.log(typeof null);
+
+//Сложные типы: обекты, массивы, функции
+// string
+// let value;
+// console.log(value, typeof value);
+// value = String(value);
+// console.log(value, typeof value);
+
+// value = 77;
+// console.log(value, typeof value);
+// value = String(value);
+// console.log(value, typeof value);
+
+// value = true;
+// console.log(value, typeof value);
+// value = String(value);
+// console.log(value, typeof value);
+
+// number
+
+let topSpeed;
+topSpeed = 160;
+console.log(topSpeed, typeof topSpeed);
+
+const x = 10;
+const y = 5;
+
+// Сложение
+console.log(x + y); // 15
+
+// Вычитание
+console.log(x - y); // 5
+
+// Умножение
+console.log(x * y); // 50
+
+// Деление
+console.log(x / y); // 2
+

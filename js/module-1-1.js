5eb1c8eec84a7fad667ff3dc019a710e46a6b761
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-07-16 01:20:24 +0300

    Update module-1-1.js
    update

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -54,6 +54,9 @@
 // console.log(value, typeof value);
 
 // number
+// alert("Hello");
+
+// confirm("JJJ&");
 
 let topSpeed;
 topSpeed = 160;
@@ -63,14 +66,57 @@ const x = 10;
 const y = 5;
 
 // Сложение
-console.log(x + y); // 15
+// console.log(x + y); // 15
 
-// Вычитание
-console.log(x - y); // 5
+// // Вычитание
+// console.log(x - y); // 5
 
-// Умножение
-console.log(x * y); // 50
+// // Умножение
+// console.log(x * y); // 50
 
-// Деление
-console.log(x / y); // 2
+// // Деление
+// console.log(x / y); // 2
 
+// alert("Hello");
+// let response = alert("Вам есть 18?");
+
+
+// let response = prompt("Введите пожалйуста число");
+// console.log(response);
+
+//Операторы
+// математические
+// console.log(2 * 4);
+// //сравнеия
+// console.log(2 >= 4);
+// //не строгое равенство
+// console.log("2" == 2);
+// // строгое равенство
+// console.log("2" === 2);
+
+//строки
+// console.log(`Hello`);
+// console.log("Hello");
+
+// const name = 'Mango';
+// console.log('My name is: ', name);
+
+// alert('Welcome!');
+
+// const isComing = confirm('PPP');
+// console.log(isComing);
+
+// alert('Attension!')
+
+// const hotelName = prompt('Please enter desired hotel name:');
+// console.log(hotelName);
+// const value = Number("xbckj");
+// console.log(Number.isNaN(value));
+// // console.log(typeof Number(value));
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(Math.floor(1.1));
+// console.log(Math.ceil(1.1));
+// console.log(Math.round(1.5));
+let age = prompt('Your age');
+console.log(age);

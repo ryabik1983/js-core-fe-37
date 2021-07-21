f4e3ca4cae6d1eaac4525168697601630a6a1e57
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-07-21 23:38:48 +0300

    update 2-1
    update 2-1

--- js/module-2-1.js
+++ js/module-2-1.js
@@ -52,7 +52,7 @@ let total = 0;
 
 
 const array = [1, 2, 3, 'JS', 4, 5, 6];
-console.table(array);
+// console.table(array);
 
 // console.log(array.length);
 
@@ -65,17 +65,19 @@ console.table(array);
 // console.log(array[1]); 
 // console.log(array[2]);
 // console.log(array);
-for(let i = 0; i < array.length; i += 1){
-        if(typeof array[i] === 'string'){
-        continue}
-        // console.log(`итерация - ${i + 1}`, array[i]);
-    
+for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] === 'string') {
+        continue
     }
-    
-for(let i = 0; i < array.length; i += 1){
+    // console.log(`итерация - ${i + 1}`, array[i]);
 
-    if(typeof array[i] === 'string'){
-    break}
+}
+
+for (let i = 0; i < array.length; i += 1) {
+
+    if (typeof array[i] === 'string') {
+        break
+    }
     // console.log(`итерация - ${i + 1}`, array[i]);
     // console.log('BREAK');
 
@@ -87,12 +89,62 @@ const multiArray = [
     [3, 30, 300, 3000],
 ]
 let it = 1;
-console.log(multiArray);
-for (let array of multiArray){
-    console.log(`iteration - ${it}`, array);
+// console.log(multiArray);
+for (let zzz of multiArray) {
+    // console.log(`iteration - ${it}`, zzz);
     it += 1;
+    for (let xxx of zzz) {
+        if (xxx === 1) {
+            // console.log(xxx);
+        }
+    }
 }
+let age = 30
+// console.log('age', age);
+let myAge = age;
+// console.log('My age', myAge);
+age = 31;
+// console.log('age', age);
+// console.log(age === myAge);
 
+const numbers = [12, 23, 34, 45];
+// console.log('numbers: ', numbers);
 
+const myNumbers = numbers;
+// console.log('myNumbers: ', myNumbers);
+numbers[0] = 100500;
+// console.log('numbers: ', numbers);
+// console.log('myNumbers: ', myNumbers);
+const message = 'Welcome to Bahamas!'
+// console.log(message.split(' '));
 
+// console.log(message.split(''));
+const clients = ['Toyota', 'BMW', 'VW'];
+// console.log(clients.join(' '));
+
+const mySliceNumbers = numbers.slice()
+// console.log(mySliceNumbers);
+// console.log(numbers === mySliceNumbers);
+numbers[0] = 12;
+// console.log(numbers);
+
+const firstTwoElems = numbers.slice(0, 2);
+// console.log(firstTwoElems);
+const myArray = ['I', 'Love', 'JS'];
+// console.log(myArray);
+let myFavourArray = myArray.join('-');
+// console.log(myFavourArray);
+
+console.log(myArray);
+console.log(numbers);
+
+//                 ADD
+// unshift(); ----------------------- push();
+//            | array               |
+//            |                     |
+//     shift() --------------------- pop();
+//                  DELETE
+
+const exampleArray = [1, 1, 2, 3, 4, 4];
+console.log(exampleArray);
 

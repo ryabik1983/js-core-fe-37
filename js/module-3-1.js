a32465c6ddb016b7fba77c3354a35ee3bd4b7e76
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-07-26 23:44:08 +0300

    update 3-1-1
    update 3-1-1

--- js/module-3-1.js
+++ js/module-3-1.js
@@ -37,7 +37,7 @@ const inputName = 'color';
 const inputValue = 'tomato';
 
 const colorPickerData = {
-[inputName]: inputValue,
+    [inputName]: inputValue,
 
 }
 // console.log(colorPickerData);
@@ -54,31 +54,40 @@ a.hello = 100;
 
 // console.log(a);
 
-const fn = function (){
+const fn = function () {
     // console.log('hello');
 }
 fn.hello = '=)';
 // console.dir(fn);
 
+// МЕТОДЫ МАССИВА!
 const playlist = {
     name: 'My super playlist',
     rating: 5,
     tracks: ['track-1', 'track-2', 'track-3', 'track-4'],
-    trackCount: 3, 
+    trackCount: 3,
     // OLD SCHOOL!!
     // getName: function (a){
     //    console.log('Ага, это getName!', a);
-        
+
     // },
     // // New SCHOOL
     // getName1 (){
     //    console.log('Так стало getName!');
     // }
-    changeName (newName) {
+    changeName(newName) {
         console.log('This inside changeName:', this);
         this.name = newName;
 
+
     },
+    addTrack(track) {
+        this.tracks.push(track);
+        this.trackCount = this.tracks.length;
+    },
+    updateRating(newRating) {
+        this.rating = newRating;
+    }
 }
 
 
@@ -86,4 +95,22 @@ const playlist = {
 // playlist.getName(5);
 // playlist.getName1();
 playlist.changeName('Новое имя!');
+playlist.addTrack('Новый трек!');
+playlist.updateRating(10);
 console.log(playlist);
+
+// ПЕРЕБОР ОБЕКТА
+
+const feedback = {
+    good: 5,
+    neutral: 10,
+    bad: 3,
+}
+// const keys = Object.keys(feedback);
+// console.log(keys);
+
+// for (const key of keys) {
+//     console.log(feedback[key]);
+// }
+const values = Object.values(feedback);
+console.log(feedback);

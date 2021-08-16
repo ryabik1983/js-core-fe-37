26b0d8316709f36dfddd7846a5ebb8bf1dfe0bab
ryabik1983 <ryabovol_saha@meta.ua>
2021-08-16 16:43:02 +0300

    6
    6

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 // import "./js/module-2-1.js"
 // import "./js/module-2-2.js"
 // import "./js/module-3-1.js"
-import "./js/module-4.js"
-import "./js/module-5.js"
+// import "./js/module-4.js"
+// import "./js/module-5.js"
+import "./js/module-6.js"
---
+++ js/module-6.js
@@ -0,0 +1,46 @@
+// const numbers = [5, 10, 15, 20, 25];
+// numbers.forEach(function(number){
+// console.log('number', number);
+// console.log(this);
+// },
+// {a: 5, b: 10},
+// );
+//          1
+const orderedItems = [5, 10, 15, 20, 25];
+function calculateTotalPrice(orderedItems) {
+    let totalPrice = 0;
+    // Пиши код ниже этой строки
+  
+    oderedItems.forEach(function(item) {
+      totalPrice += item;
+    });
+    
+  
+    // Пиши код выше этой строки
+    return totalPrice;
+    console.log(totalPrice);
+  }
+  calculateTotalPrice();
+  
+
+
+// const orderedItems = [5, 10, 15, 20, 25];
+
+// const doubleNums = orderedItems.map (function(item){
+//     console.log(item);
+//     return 555;
+// });
+
+// console.log(doubleNums);
+// orderedItems.forEach(calculateTotalPrice(item),
+// {
+//     let totalPrice = 0;,
+//     totalPrice += item;
+// }
+// // return totalPrice;
+// )
+// console.log(totalPrice); 
+   
+//     // Пиши код ниже этой строки
+  
+    

8c916d612f0dc47fdc2d2cff3767704865c638a8
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-08-17 19:00:51 +0300

    6
    6

--- js/module-6.js
+++ js/module-6.js
@@ -6,41 +6,51 @@
 // {a: 5, b: 10},
 // );
 //          1
-const orderedItems = [5, 10, 15, 20, 25];
-function calculateTotalPrice(orderedItems) {
-    let totalPrice = 0;
-    // Пиши код ниже этой строки
-  
-    oderedItems.forEach(function(item) {
-      totalPrice += item;
-    });
-    
-  
-    // Пиши код выше этой строки
-    return totalPrice;
-    console.log(totalPrice);
-  }
-  calculateTotalPrice();
-  
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Пиши код ниже этой строки
 
-
-// const orderedItems = [5, 10, 15, 20, 25];
-
-// const doubleNums = orderedItems.map (function(item){
-//     console.log(item);
-//     return 555;
-// });
-
-// console.log(doubleNums);
-// orderedItems.forEach(calculateTotalPrice(item),
-// {
-//     let totalPrice = 0;,
+//   orderedItems.forEach(item => {
 //     totalPrice += item;
+//   });
+
+
+//   // Пиши код выше этой строки
+//   return console.log(totalPrice);
 // }
-// // return totalPrice;
-// )
-// console.log(totalPrice); 
-   
-//     // Пиши код ниже этой строки
-  
-    
+// calculateTotalPrice([12, 85, 37, 4]);
+// calculateTotalPrice([164, 48, 291]);
+
+//        2
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Пиши код ниже этой строки
+
+//   numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Пиши код выше этой строки
+//   return console.log(filteredNumbers);
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+
+// const users = [
+//   { id: '000', name: 'Mango', isActive: true },
+//   { id: '001', name: 'Poly', isActive: false },
+//   { id: '002', name: 'Ajax', isActive: true },
+//   { id: '003', name: 'Chelsey', isActive: false },
+// ];
+
+// const userNames = users.map(user => user.name);
+// console.log(userNames);
+// const userId = users.map(user => user.id);
+// console.log(userId);
+
+
+

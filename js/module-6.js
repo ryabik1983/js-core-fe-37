cfce0b1847a56959cb59fe60038d71e23d37690a
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-08-19 01:17:28 +0300

    6
    6

--- js/module-6.js
+++ js/module-6.js
@@ -53,19 +53,19 @@
 // console.log(userId);
 
 //          3
-function getCommonElements(firstArray, secondArray) {
-    const commonElements = [];
-    // Пиши код ниже этой строки
-    firstArray.forEach((element) => {
-        if (secondArray.includes(element)) {
-            commonElements.push(element);
-        };
-    });
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+//     // Пиши код ниже этой строки
+//     firstArray.forEach((element) => {
+//         if (secondArray.includes(element)) {
+//             commonElements.push(element);
+//         };
+//     });
 
-    console.log(commonElements);
+//     console.log(commonElements);
 
-    // Пиши код выше этой строки
-}
+// Пиши код выше этой строки
+// }
 // getCommonElements([1, 2, 3], [2, 4]);
 // getCommonElements([1, 2, 3], [2, 1, 17, 19])
 // getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])
@@ -98,11 +98,109 @@ function getCommonElements(firstArray, secondArray) {
 //     console.log(filteredNumbers);
 //     // return filteredNumbers;
 // }
-// filterArray([1, 2, 3, 4, 5], 3)
-const numbers = [5, 10, 15, 20, 25];
-const total = numbers.reduce((acc, value) => {
-    console.log('number', value);
-    console.log('total', acc);
-    return acc + value;
-}, 0);
-console.log(total);
+// // filterArray([1, 2, 3, 4, 5], 3)
+// const numbers = [5, 10, 15, 20, 25];
+// const total = numbers.reduce((acc, value) => {
+//     console.log('number', value);
+//     console.log('total', acc);
+//     return acc + value;
+// }, 0);
+// console.log(total);
+
+//          9
+
+// function changeEven(numbers, value) {
+//     // Пиши код ниже этой строки
+//     const newNumbers = numbers.map(function (num) {
+
+//         if (num % 2 === 0) {
+//             return num = num + value;
+//         }
+//         return num;
+
+//     });
+//     console.log(numbers);
+//     console.log(newNumbers);
+// };
+
+// function changeEven(numbers, value) {
+//     // Пиши код ниже этой строки
+//     const newNumbers = [];
+//     numbers.forEach(function (num) {
+
+//         if (num % 2 === 0) {
+//             return newNumbers.push(num + value);
+//         }
+//         else { return newNumbers.push(num) };
+
+
+//     });
+//     console.log(numbers);
+//     console.log(newNumbers);
+// };
+
+
+// function changeEven(numbers, value) {
+//     // Пиши код ниже этой строки
+//     const newNumbers = [];
+//     for (let i = 0; i < numbers.length; i += 1) {
+//         if (numbers[i] % 2 === 0) {
+//             newNumbers.push(numbers[i] + value);
+//         }
+//         else { newNumbers.push(numbers[i]) };
+
+//     }
+//     console.log(numbers);
+//     console.log(newNumbers);
+//     // Пиши код выше этой строки
+// }
+
+// changeEven([1, 2, 3, 4, 5], 10);
+// changeEven([2, 8, 3, 7, 4, 6], 10);
+// changeEven([17, 24, 68, 31, 42], 100);
+// changeEven([15, 8, 10, 11, 9, 22, 11, 19, 18, 20, 24, 9, 14, 12, 15, 5, 12, 14, 7, 13, 11, 13, 24], 23);
+
+//          10
+
+// const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+// // Пиши код ниже этой строки
+// const planetsLengths = planets.map(planet =>
+//     planet.length
+// );
+// console.log(planetsLengths);
+
+//          11
+// const books = [
+//     { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//     { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//     { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//     { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//     { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
+// // Пиши код ниже этой строки
+
+// const titles = books.map(book => book.title);
+// console.log(titles);
+//          12
+
+const books = [
+    {
+        title: 'Последнее королевство',
+        author: 'Бернард Корнуэлл',
+        genres: ['приключения', 'историческое']
+    },
+    {
+        title: 'На берегу спокойных вод',
+        author: 'Роберт Шекли',
+        genres: ['фантастика']
+    },
+    {
+        title: 'Красна как кровь',
+        author: 'Ли Танит',
+        genres: ['ужасы', 'мистика']
+    }
+];
+// Пиши код ниже этой строки
+
+const genres = books.flatMap(book => book.genres);
+console.log(genres);

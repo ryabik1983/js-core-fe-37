a3ec3cb87fc9b0d1dabf6dd32eb1609c4b23ffc2
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-08-22 10:34:08 +0300

    6
    6

--- js/module-6.js
+++ js/module-6.js
@@ -222,95 +222,197 @@
 // const allCourses = students.flatMap(student => student.courses);
 // const uniqueCourses = allCourses.filter(
 //     (course, index, array) => array.indexOf(course) === index
-// // );
-// const users = [
-//     {
-//         name: 'Moore Hensley',
-//         email: 'moorehensley@indexia.com',
-//         eyeColor: 'blue',
-//         friends: ['Sharron Pace'],
-//         isActive: false,
-//         balance: 2811,
-//         gender: 'male',
-//         age: 37
-//     },
-//     {
-//         name: 'Sharlene Bush',
-//         email: 'sharlenebush@tubesys.com',
-//         eyeColor: 'blue',
-//         friends: ['Briana Decker', 'Sharron Pace'],
-//         isActive: true,
-//         balance: 3821,
-//         gender: 'female',
-//         age: 34
-//     },
-//     {
-//         name: 'Ross Vazquez',
-//         email: 'rossvazquez@xinware.com',
-//         eyeColor: 'green',
-//         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//         isActive: false,
-//         balance: 3793,
-//         gender: 'male',
-//         age: 24
-//     },
-//     {
-//         name: 'Elma Head',
-//         email: 'elmahead@omatom.com',
-//         eyeColor: 'green',
-//         friends: ['Goldie Gentry', 'Aisha Tran'],
-//         isActive: true,
-//         balance: 2278,
-//         gender: 'female',
-//         age: 21
-//     },
-//     {
-//         name: 'Carey Barr',
-//         email: 'careybarr@nurali.com',
-//         eyeColor: 'blue',
-//         friends: ['Jordan Sampson', 'Eddie Strong'],
-//         isActive: true,
-//         balance: 3951,
-//         gender: 'male',
-//         age: 27
-//     },
-//     {
-//         name: 'Blackburn Dotson',
-//         email: 'blackburndotson@furnigeer.com',
-//         eyeColor: 'brown',
-//         friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//         isActive: false,
-//         balance: 1498,
-//         gender: 'male',
-//         age: 38
-//     },
-//     {
-//         name: 'Sheree Anthony',
-//         email: 'shereeanthony@kog.com',
-//         eyeColor: 'brown',
-//         friends: ['Goldie Gentry', 'Briana Decker'],
-//         isActive: true,
-//         balance: 2764,
-//         gender: 'female',
-//         age: 39
-//     }
-// ];
-// console.log(users);
-// const getFriends = (users) => {
-//     let allFriends = users.flatMap(user => user.friends);
-//     return allFriends;
-// }
-
-// console.log(allFriends);
-
-class Samurai {
-    constructor(name) {
-        this.name = name;
+// // // );
+const users = [
+    {
+        name: 'Moore Hensley',
+        email: 'moorehensley@indexia.com',
+        eyeColor: 'blue',
+        friends: ['Sharron Pace'],
+        isActive: false,
+        balance: 2811,
+        gender: 'male',
+        age: 37
+    },
+    {
+        name: 'Sharlene Bush',
+        email: 'sharlenebush@tubesys.com',
+        eyeColor: 'blue',
+        friends: ['Briana Decker', 'Sharron Pace'],
+        isActive: true,
+        balance: 3821,
+        gender: 'female',
+        age: 34
+    },
+    {
+        name: 'Ross Vazquez',
+        email: 'rossvazquez@xinware.com',
+        eyeColor: 'green',
+        friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+        isActive: false,
+        balance: 3793,
+        gender: 'male',
+        age: 24
+    },
+    {
+        name: 'Elma Head',
+        email: 'elmahead@omatom.com',
+        eyeColor: 'green',
+        friends: ['Goldie Gentry', 'Aisha Tran'],
+        isActive: true,
+        balance: 2278,
+        gender: 'female',
+        age: 21
+    },
+    {
+        name: 'Carey Barr',
+        email: 'careybarr@nurali.com',
+        eyeColor: 'blue',
+        friends: ['Jordan Sampson', 'Eddie Strong'],
+        isActive: true,
+        balance: 3951,
+        gender: 'male',
+        age: 27
+    },
+    {
+        name: 'Blackburn Dotson',
+        email: 'blackburndotson@furnigeer.com',
+        eyeColor: 'brown',
+        friends: ['Jacklyn Lucas', 'Linda Chapman'],
+        isActive: false,
+        balance: 1498,
+        gender: 'male',
+        age: 38
+    },
+    {
+        name: 'Sheree Anthony',
+        email: 'shereeanthony@kog.com',
+        eyeColor: 'brown',
+        friends: ['Goldie Gentry', 'Briana Decker'],
+        isActive: true,
+        balance: 2764,
+        gender: 'female',
+        age: 39
     }
-    hello() { alert(this.name) }
-};
-console.log(({}).prototype === ({}).__proto__);
-let shogun = new Samurai('Dimmich');
-console.log(shogun.__proto__.__proto__);
-console.log(shogun.__proto__.constructor.__proto__);
-console.log(shogun.__proto__.__proto__.__proto__);
+];
+// console.log(users);
+// const allFriends = users.flatMap(user => user.friends).filter((el, index, arr) => arr.indexOf(el) === index);
+// console.log(allFriends);
+//          22
+
+// const activeUsers = users.filter(user => user.isActive === true);
+// console.log(activeUsers);
+//          23
+// const getInactiveUsers = users.filter(user => user.isActive === false);
+// console.log(getInactiveUsers);
+//          24
+// const books = [
+//     { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//     { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//     { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//     { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'Сон смешного человека';
+// const AUTHOR = 'Роберт Шекли';
+// // Пиши код ниже этой строки
+
+// const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
+// console.log(bookWithTitle);
+// const bookByAuthor = books.find(book => book.author === AUTHOR);
+// console.log(bookByAuthor);
+//          26
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Пиши код ниже этой строки
+
+// // const eachElementInFirstIsEven = firstArray.every(element => element % 2 === 0);
+
+// // const eachElementInFirstIsEven = firstArray.every(element => element % 2 === 0);
+// // console.log(eachElementInFirstIsEven);
+// const eachElementInFirstIsOdd = firstArray.every(element => element % 2 !== 0);
+// console.log(eachElementInFirstIsOdd);
+
+//          27
+
+// const activeUsers = users.every(user => user.isActive === true);
+// console.log(activeUsers);
+// const unactiveUsers = users.every(user => user.isActive === false);
+// console.log(unactiveUsers);
+//          28
+// const anyElementInFirstIsEven = firstArray.some(elem => elem % 2 === 0);
+// console.log(anyElementInFirstIsEven);
+
+// const anyElementInFirstIsOdd = firstArray.some(elem => elem % 2 !== 0);
+// console.log(anyElementInFirstIsOdd);
+
+// const anyElementInSecondIsEven = secondArray;
+// const anyElementInSecondIsOdd = secondArray;
+
+// const anyElementInThirdIsEven = thirdArray;
+// const anyElementInThirdIsOdd = thirdArray;
+//          30
+
+// const players = {
+//     mango: 1270,
+//     poly: 468,
+//     ajax: 710,
+//     kiwi: 244
+// };
+// const playtimes = Object.values(players);
+
+// console.log(playtimes);
+// // [1270, 468, 710, 244]
+// // Пиши код ниже этой строки
+
+// const totalPlayTime = playtimes.reduce((acc, value) => { console.log(acc += value); return acc }, 0);
+// console.log(totalPlayTime);
+
+// // Пиши код выше этой строки
+// const averagePlayTime = totalPlayTime / playtimes.length;
+// console.log(averagePlayTime);
+//          31
+// const players = [
+//     { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
+//     { name: 'Поли', playtime: 469, gamesPlayed: 2 },
+//     { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
+//     { name: 'Киви', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Пиши код ниже этой строки
+
+// const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+//     total += player.playtime / player.gamesPlayed;
+//     return total
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
+//          32
+
+// const calculateTotalBalance = users.reduce((total, user) => {
+//     total += user.balance;
+//     return total;
+// }, 0);
+// console.log(calculateTotalBalance);
+//          33
+const getTotalFriendCount = users.reduce((allFriends, user) => {
+    allFriends.push(...user.friends);
+    // return allFriends;
+    return allFriends;
+}, []);
+console.log(getTotalFriendCount);
+
+
+
+
+
+// class Samurai {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     hello() { alert(this.name) }
+// };
+// console.log(({}).prototype === ({}).__proto__);
+// let shogun = new Samurai('Dimmich');
+// console.log(shogun.__proto__.__proto__);
+// console.log(shogun.__proto__.constructor.__proto__);
+// console.log(shogun.__proto__.__proto__.__proto__);

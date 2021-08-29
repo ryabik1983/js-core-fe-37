69c83746441ad863fcdf277286a6ceea376c71e4
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-08-30 00:15:55 +0300

    7
    7

--- js/module-7.js
+++ js/module-7.js
@@ -10,20 +10,36 @@
 // navItemEl.appendChild(navLinkEl);
 // console.log(navItemEl);
 const selectedById = document.querySelector('#menu');
-selectedById.style.textTransform = 'uppercase';
-selectedById.style.fontSize = '2rem';
+// selectedById.style.textTransform = 'uppercase';
+// selectedById.style.fontSize = '2rem';
 console.log(selectedById); // ...
 
 const menuItemsByTagName = document.querySelectorAll("li");
 console.log(menuItemsByTagName); // ...
 
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass); // ...
+// const menuItemsByClass = document.querySelectorAll(".menu-item-2");
+// console.log(menuItemsByClass); // ...
 
 const firstMenuItem = document.querySelector(".menu-item");
 firstMenuItem.style.color = '#BF4E30';
 console.log(firstMenuItem); // ...
 const menuItem = document.querySelector(".menu-item-1");
 console.log(menuItem);
-const navEl = document.querySelector('ul');
-console.dir(navEl);
+// const navEl = document.querySelector('ul');
+// console.dir(navEl);
+const navEl = menu.querySelector("li");
+console.log(navEl);
+const heroTitleEl = document.querySelector('.hero_title');
+console.log(heroTitleEl);
+console.log(heroTitleEl.textContent);
+heroTitleEl.textContent = 'How do you do?';
+const actions = document.querySelectorAll('.js-actions button');
+console.log(actions[1].dataset.action);
+const inputEl = document.querySelector('.js-input');
+
+inputEl.value = 'qweqwe';
+console.log(inputEl.value);
+const menuEl = document.querySelector('.menu');
+console.log(menuEl.classList);
+menuEl.classList.add('super-cool');
+console.log(menuEl.classList);

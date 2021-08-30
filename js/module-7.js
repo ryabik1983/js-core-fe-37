961f041b6ec9c19d7e44f3524f1b95bcaca7bd3c
ryabik1983 <ryabovol_saha@meta.ua>
2021-08-30 16:34:33 +0300

    7
    7

--- js/module-7.js
+++ js/module-7.js
@@ -9,37 +9,75 @@
 // console.log(navLinkEl);
 // navItemEl.appendChild(navLinkEl);
 // console.log(navItemEl);
-const selectedById = document.querySelector('#menu');
-// selectedById.style.textTransform = 'uppercase';
-// selectedById.style.fontSize = '2rem';
-console.log(selectedById); // ...
+// const selectedById = document.querySelector('#menu');
+// // selectedById.style.textTransform = 'uppercase';
+// // selectedById.style.fontSize = '2rem';
+// console.log(selectedById); // ...
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName); // ...
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName); // ...
 
-// const menuItemsByClass = document.querySelectorAll(".menu-item-2");
-// console.log(menuItemsByClass); // ...
+// // const menuItemsByClass = document.querySelectorAll(".menu-item-2");
+// // console.log(menuItemsByClass); // ...
 
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = '#BF4E30';
-console.log(firstMenuItem); // ...
-const menuItem = document.querySelector(".menu-item-1");
-console.log(menuItem);
-// const navEl = document.querySelector('ul');
-// console.dir(navEl);
-const navEl = menu.querySelector("li");
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = '#BF4E30';
+// console.log(firstMenuItem); // ...
+// const menuItem = document.querySelector(".menu-item-1");
+// console.log(menuItem);
+// // const navEl = document.querySelector('ul');
+// // console.dir(navEl);
+// const navEl = menu.querySelector("li");
+// console.log(navEl);
+// const heroTitleEl = document.querySelector('.hero_title');
+// console.log(heroTitleEl);
+// console.log(heroTitleEl.textContent);
+// heroTitleEl.textContent = 'How do you do?';
+// const actions = document.querySelectorAll('.js-actions button');
+// console.log(actions[1].dataset.action);
+// const inputEl = document.querySelector('.js-input');
+
+// inputEl.value = 'qweqwe';
+// console.log(inputEl.value);
+// const menuEl = document.querySelector('.menu');
+// console.log(menuEl.classList);
+// menuEl.classList.add('super-cool');
+// console.log(menuEl.classList);
+// menuEl.classList.toggle('fusk');
+// console.log(menuEl.classList);
+// // menuEl.classList.toggle('fusk');
+// // console.log(menuEl.classList);
+// menuEl.classList.replace('fusk', 'fuck');
+// console.log(menuEl.classList);
+// console.log(menuEl.classList.contains('fuck'));
+// console.log(menuEl.classList.contains('fusk'));
+
+// const currentPageUrl = '/blog';
+// const linkEl = document.querySelector(`.menu-item-link[href="${currentPageUrl}"]`);
+// console.log(linkEl);
+// linkEl.classList.add('menu-item-link__current');
+// console.log(linkEl);
+
+const navEl = document.querySelector('.menu');
 console.log(navEl);
-const heroTitleEl = document.querySelector('.hero_title');
-console.log(heroTitleEl);
-console.log(heroTitleEl.textContent);
-heroTitleEl.textContent = 'How do you do?';
-const actions = document.querySelectorAll('.js-actions button');
-console.log(actions[1].dataset.action);
-const inputEl = document.querySelector('.js-input');
+// const firstNavEl = navEl.firstElementChild;
+// console.log(firstNavEl);
+const navElFirsrChild = navEl.firstChild;
+console.log(navElFirsrChild);
 
-inputEl.value = 'qweqwe';
-console.log(inputEl.value);
-const menuEl = document.querySelector('.menu');
-console.log(menuEl.classList);
-menuEl.classList.add('super-cool');
-console.log(menuEl.classList);
+const navElLastChild = navEl.lastChild;
+console.log(navElLastChild);
+
+const navElementLastChild = navEl.lastElementChild;
+console.log(navElementLastChild);
+
+const navElementFirstChild = navEl.firstElementChild;
+console.log(navElementFirstChild); 
+const menuItem2 = document.querySelector('.menu-item-2');
+console.log(menuItem2);
+const prevSib = menuItem2.previousElementSibling;
+console.log(prevSib);
+const nextSib = menuItem2.nextElementSibling;
+console.log(nextSib);
+
+

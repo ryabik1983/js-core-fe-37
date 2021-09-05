4fde786ca485f47c2802a1374c8f837a86a8a7da
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-09-05 21:00:20 +0300

    7
    7

--- js/module-7.js
+++ js/module-7.js
@@ -187,3 +187,11 @@ decrementBtn.addEventListener('click', decrement);
 // function onTargetButtonClick() {
 
 // };
+const input = document.querySelector('#name-input');
+console.log(input);
+const output = document.querySelector('#name-output');
+console.log(output);
+input.addEventListener('input', () => {
+
+});
+

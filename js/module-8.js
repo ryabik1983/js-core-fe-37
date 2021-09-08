37f84379d67d5fe644199fe783b7269626d002d8
Alexandr Ryabovol <ryabovol_saha@meta.ua>
2021-09-09 01:21:10 +0300

    module-8
    module-8

--- js/module-8.js
+++ js/module-8.js
@@ -51,36 +51,37 @@
 //     elem.textContent = `<a href="#" class="btn">Дополнительная ${elCounter}</a>`;
 //     nav.appendChild(elem);
 // }
-const tagsContainer = document.querySelector('.js-nav');
-// let selectedBtn = null;
-console.log(tagsContainer);
-const selectedTags = new Set();
-tagsContainer.addEventListener('click', ontagsContainerClick);
+// const tagsContainer = document.querySelector('.js-nav');
+// // let selectedBtn = null;
+// console.log(tagsContainer);
+// const selectedTags = new Set();
+// tagsContainer.addEventListener('click', ontagsContainerClick);
 
-function ontagsContainerClick(evt) {
-    if (evt.target.nodeName !== "BUTTON") {
-        return;
-           }
-// console.log(evt.target.textContent);
-const btn = evt.target;
-const tag = btn.dataset.value;
-const isActive = btn.classList.contains('active');
-if(isActive){
-    selectedTags.delete(tag)}
-    else{
-        selectedTags.add(tag)
-    }
+// function ontagsContainerClick(evt) {
+//   if (evt.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+//   // console.log(evt.target.textContent);
+//   const btn = evt.target;
+//   const tag = btn.dataset.value;
+//   const isActive = btn.classList.contains('active');
+//   if (isActive) {
+//     selectedTags.delete(tag)
+//   }
+//   else {
+//     selectedTags.add(tag)
+//   }
 
 
-btn.classList.toggle('active')
-console.log(selectedTags);
+//   btn.classList.toggle('active')
+//   console.log(selectedTags);
 
-// selectedTags.add(btn.dataset.value)
-// console.log(selectedTags);
-// console.log(selectedTags.size);
-// console.log(selectedTags.has('css'));
+//   // selectedTags.add(btn.dataset.value)
+//   // console.log(selectedTags);
+//   // console.log(selectedTags.size);
+//   // console.log(selectedTags.has('css'));
 
-}
+// }
 
 
 //     const currentActiveBtn = document.querySelector('.active');
@@ -119,106 +120,117 @@ console.log(selectedTags);
 // }
 
 const galleryItems = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-      description: 'Container Haulage Freight',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-      description: 'Aerial Beach View',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-      description: 'Flower Blooms',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-      description: 'Alpine Mountains',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-      description: 'Mountain Lake Sailing',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-      description: 'Alpine Spring Meadows',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-      description: 'Nature Landscape',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-      description: 'Lighthouse Coast Sea',
-    },
-  ];
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+const galleryList = document.querySelector('.js-gallery');
+// console.log(galleryList);
+// Вешаем слушателя события на список
+galleryList.addEventListener('click', onGalleryListClick)
+function onGalleryListClick(evt) {
+  //     // Фильтр цели клика
+  const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+  if (!isGalleryImageEl) {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+};
 
-// <ul class = "js-gallery">
-//         <li class="gallery__item">
-//             <a
-//               class="gallery__link"
-//               href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//             >
-//               <img
-//                 class="gallery__image"
-//                 src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-//                 data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//                 alt="Tulips"
-//               />
-//             </a>
-//           </li>
-//     </ul>
-function createGalleryCardsMarkup (items) {
-const markup = items.map(item => {
-    return
-    `<ul class = "js-gallery">
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+// console.log(galleryMarkup);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup)
+
+// console.log(createGalleryCardsMarkup(galleryItems));
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    
     <li class="gallery__item">
         <a
           class="gallery__link"
-          href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+          href="${original}"
         >
           <img
             class="gallery__image"
-            src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-            alt="Tulips"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
           />
         </a>
       </li>
-    </ul>`
-})
-console.log(markup);
+    `;
+    }
+    )
+    .join('');
 }
 
-createGalleryCardsMarkup (galleryItems);
+
+
+
+// const markup = galleryItems.map(item =>
+//   `Это метод!`
+// );
+// console.log(markup);
+

c75b453d7e7dbd8087d94307f43051fdbf067810
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-09-18 21:58:23 +0300

    9
    9

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const sum = (a, b) => a + b;
+
+sum (2, 3);
+
+class Hero {
+    constructor (name) {
+        this._name = name;
+    }
+get name () {
+        return this._name;
+        }
+set name (newName){
+            this._name = newName;
+        } 
+
+}
+
+// const h = new Hero ('mango');
+// h.sayHi() {
+//     user.set(this.name = name)
+// },
